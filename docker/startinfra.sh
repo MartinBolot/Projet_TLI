e2059ab3118059ffd97dc579eaeef370a3faa15d
John-Pi <john.picolton@gmail.com>
2018-05-26 22:08:26 +0200

    pipapo

--- docker/startinfra.sh
+++ docker/startinfra.sh
@@ -1,7 +1,7 @@
 sudo service apache2 stop
-chown -R www-data:www-data /root/www
 cd /root/www/
-sudo -u www-data php /root/composer.phar install
+sudo php /root/composer.phar install
+chown -R www-data:www-data /root/www
 cd /root/www/docker/
 sudo docker-compose build www
 sudo docker-compose up -d
