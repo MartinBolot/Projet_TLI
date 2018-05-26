896271ba0f6ded009c44e0858fd8c902848f61bb
John-Pi <john.picolton@gmail.com>
2018-05-26 22:01:58 +0200

    lole

--- docker/startinfra.sh
+++ docker/startinfra.sh
@@ -1,5 +1,6 @@
 sudo service apache2 stop
 chown -R www-data:www-data /root/www
+sudo -u www-data php ../composer.phar install
 cd /root/www/docker/
 sudo docker-compose build www
 sudo docker-compose up -d
