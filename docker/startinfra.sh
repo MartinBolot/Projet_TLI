c5a13c0284ebfb4a3893803bb5382491f1a7e50b
John-Pi <john.picolton@gmail.com>
2018-05-26 22:04:51 +0200

    uber boost

--- docker/startinfra.sh
+++ docker/startinfra.sh
@@ -1,6 +1,7 @@
 sudo service apache2 stop
 chown -R www-data:www-data /root/www
-sudo -u www-data php ../composer.phar install
+cd /root/www/
+sudo -u www-data php /root/composer.phar install
 cd /root/www/docker/
 sudo docker-compose build www
 sudo docker-compose up -d
